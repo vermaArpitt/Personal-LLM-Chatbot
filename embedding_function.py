9ded5106d169bf6f055596ecb6f78ac02ad6759e
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-09-26 09:26:41 +0530

    Completed query requesting and receiving response

--- embedding_function.py
+++ embedding_function.py
@@ -1,5 +1,5 @@
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 
 def get_embedding_function():
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    embeddings = OllamaEmbeddings(model="mxbai-embed-large")
     return embeddings 
